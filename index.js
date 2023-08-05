39e2f8bda11e22b30baf41dd8d218f625ffc505e
Brenny Souza <106251815+brennysouza@users.noreply.github.com>
2023-08-05 19:58:46 -0400

    added some copied code from previous class to get array started, will revise for this challenge

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 // TODO: Include packages needed for this application
+// Inquirer package included in package.json
 
 // TODO: Create an array of questions for user input
 const questions = [];
+
+// const inquirer = require('inquirer');
+
+questions
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your user name?',
+      name: 'username',
+    },
+    {
+      type: 'password',
+      message: 'What is your password?',
+      name: 'password',
+    },
+    {
+      type: 'password',
+      message: 'Re-enter password to confirm:',
+      name: 'confirm',
+    },
+  ])
+  .then((response) =>
+    response.confirm === response.password
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
